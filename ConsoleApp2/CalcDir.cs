290425bec7749fdd909ee2f34ebda84012b24abd
basicomCsharp <coolandrey@gmail.com>
2022-09-02 20:50:34 +0500

    Модуль 8. Task3. Создание.

---
+++ ConsoleApp2/CalcDir.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task2
+{
+    /// <summary>
+    /// Класс для подсчёта размера и количества файлов в каталоге
+    /// </summary>
+    class CalcDir
+    {     
+        /// <summary>
+        /// Размер каталога
+        /// </summary>
+        public long SizeDir { get; set; }
+        /// <summary>
+        /// Количество файлов
+        /// </summary>
+        public long CounFile { get; set; }
+        /// <summary>
+        /// Метод подсчёта количества файлов в каталоге и размер
+        /// </summary>
+        /// <param name="dir">путь</param>
+        /// <returns></returns>
+        public long GetSizeDirAndCountFiles(string dir)
+        {            
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(dir);
+                if (dirInfo.Exists)
+                {
+                    CounFile += dirInfo.GetFiles().Length;                    
+                    foreach (FileInfo f in dirInfo.GetFiles())
+                    {
+                        SizeDir += f.Length;                        
+                    }
+                    string[] dirs = Directory.GetDirectories(dir);
+                    foreach (string d in dirs)
+                    {
+                        SizeDir = GetSizeDirAndCountFiles(d);
+                    }
+                    return SizeDir;
+                }
+                return SizeDir;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);                
+                return SizeDir;
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)            
+        {
+            string sDir = "";
+            Console.WriteLine("Введите каталог для подсчёта файлов");
+            sDir = Console.ReadLine();
+
+            CalcDir classCalcDir = new CalcDir();
+            var sizeDir = classCalcDir.GetSizeDirAndCountFiles(sDir);            
+            Console.WriteLine("Каталог {0} содержит {1} файлов общий объём которых равен в байтах : {2}", sDir, classCalcDir.CounFile, sizeDir);
+            Console.ReadKey();
+        }
+    }
+}
